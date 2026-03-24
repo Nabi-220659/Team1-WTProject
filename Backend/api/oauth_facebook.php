076580dbef4ad2b1c3aaedc6b3bef18b6b1548e8
Nabi-220659 <n220659@rguktn.ac.in>
2026-03-24 16:17:30 +0530

    Oauth Files Uploaded (closes #61)

---
+++ Backend/api/oauth_facebook.php
@@ -0,0 +1,24 @@
+<?php
+// oauth_facebook.php
+
+$clientId = 'YOUR_FACEBOOK_APP_ID';
+$redirectUri = 'http://localhost/Loan-Management/Backend/api/oauth_facebook_callback.php';
+$scope = 'email,public_profile';
+
+$state = bin2hex(random_bytes(16));
+session_start();
+$_SESSION['oauth_state_fb'] = $state;
+
+$authUrl = 'https://www.facebook.com/v16.0/dialog/oauth?' . http_build_query([
+    'client_id' => $clientId,
+    'redirect_uri' => $redirectUri,
+    'state' => $state,
+    'scope' => $scope
+]);
+
+// Since this is a mockup/dev environment, we'll simulate the redirect back immediately
+$mockCode = 'MOCK_FB_AUTH_CODE_' . rand(1000, 9999);
+$mockCallbackUrl = $redirectUri . '?code=' . $mockCode . '&state=' . $state;
+
+header('Location: ' . $mockCallbackUrl);
+exit;
